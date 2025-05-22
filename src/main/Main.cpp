cd8a16fa9e4410ed74155dc0644e8de1a5a8afea
Thrent <1279514405@qq.com>
2025-05-22 22:59:11 +0800

    @Update 1. 修改宏

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -1,6 +1,7 @@
-#include "ex/stackback.h"
 #include <iostream>
 #include <stdexcept>
+
+#include "ex/stackback.h"
 
 // UTF-8 测试
 // 当你看到这串中文的时候就证明UTF-8生效了
@@ -8,14 +9,11 @@
 void foo() { throw std::runtime_error("foo failed"); }
 
 int main(int argc, char const* argv[]) {
-
-    CPPTRACE_TRY {
-        foo();
-    }
+    CPPTRACE_TRY { foo(); }
     CPPTRACE_CATCH(const std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
-        PrintCurrentStack();
-        
+        auto strings = StringStackBack();
+        std::cerr << strings << std::endl;
     }
 
     return 0;
--- src/main/include/ex/stackback.h
+++ src/main/include/ex/stackback.h
@@ -1,14 +1,20 @@
 #ifndef EX_STACKBACK_H
-#define EX_STACKBACK_H  
+#define EX_STACKBACK_H
 
-#include <cpptrace/from_current.hpp>
 #include <cpptrace/formatting.hpp>
-
-#define PrintCurrentStack() \
-    cpptrace::formatter{}   \
-    .header("Stack trace:") \
-    .addresses(cpptrace::formatter::address_mode::object)   \
-    .print(cpptrace::from_current_exception())
+#include <cpptrace/from_current.hpp>
 
 
-#endif // EX_STACKBACK_H
+#define PrintStackBack()                                      \
+    cpptrace::formatter{}                                     \
+        .header("Stack trace:")                               \
+        .addresses(cpptrace::formatter::address_mode::object) \
+        .print(cpptrace::from_current_exception())
+
+#define StringStackBack()                                     \
+    cpptrace::formatter{}                                     \
+        .header("Stack trace:")                               \
+        .addresses(cpptrace::formatter::address_mode::object) \
+        .format(cpptrace::from_current_exception())
+
+#endif  // EX_STACKBACK_H
